6b51d7ef63b890ffb33012d8c38baed83ce3a193
kolluriabhishek <kolluriabhishek54@gmail.com>
2023-12-04 22:37:49 +0530

    Intial commit tick tac toe

---
+++ Tick_tac_toe/main.py
@@ -0,0 +1,19 @@
+def lengthOfLongestSubstring(s: str) -> int:
+    c = 1
+    maxc = 0
+    if (len(s) == 0):
+        return 0
+    for i in range(1, len(s)):
+        if (s[i] not in s[i - c:i]):
+            c = c + 1
+            if (maxc < c):
+                maxc = c
+                print(s[i],end='----')
+                print(s[i-c:i],end='----')
+                print(maxc)
+        else:
+            c = 1
+    if (maxc == 0):
+        maxc = 1
+    return maxc
+lengthOfLongestSubstring("dvdf")
